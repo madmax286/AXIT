f3de12410a8fed5c263a0a1edb998765b66ac34a
madmax286 <mistermax@tut.by>
2023-04-20 02:57:07 +0300

    customise pagination

--- js/script.js
+++ js/script.js
@@ -1,19 +1,9 @@
-
-
 const swiper = new Swiper('.swiper', {
   loop: true,
 
   pagination: {
     el: '.swiper-pagination',
-  },
-
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  scrollbar: {
-    el: '.swiper-scrollbar',
+    clickable: true,
   },
 
   slidesPerView: 3,
